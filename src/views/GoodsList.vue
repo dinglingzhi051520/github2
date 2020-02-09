a599e69170fb280a51c905b7c5aa5a2ba815308e
dinglingzhi051520 <949409106@qq.com>
2020-02-09 14:01:37 +0800

    F:\React\chu

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="">
-    <nav-header></nav-header>
+    <nav-header></nav-header> <!--头部组件-->
     <nav-bread>
-      <span>Goods</span>
+      <span>Goods</span>   <!--面包学-->
     </nav-bread>
 
     <div class="accessory-result-page accessory-page">
@@ -18,9 +18,9 @@
       <div class="filter stopPop" id="filter" v-bind:class="{'filterby-show':filterBy}">
         <dl class="filter-price">
           <dt>Price:</dt>
-          <dd><a href="javascript:void(0)" v-bind:class="{'cur':priceChecked=='all'}" @click="priceChecked=all">All</a></dd>
-          <dd v-for="(price,index) in priceFilter" :key="index">
-            <a href="javascript:void(0)" @click="setPriceFilter(index)" v-bind:class="{'cur':priceChecked==index}">{{price.startPrice}}-{{price.endPrice}}</a>
+          <dd><a href="javascript:void(0)" v-bind:class="{'cur':priceChecked=='all'}" @click="priceChecked=all">All</a></dd><!--动态的绑定class,让他定义的数等于All,绑定点击事件使得点击的时候让定义的数和All相等-->
+          <dd v-for="(price,index) in priceFilter" :key="index"> <!--循环价格渲染到页面-->
+            <a href="javascript:void(0)" @click="setPriceFilter(index)" v-bind:class="{'cur':priceChecked==index}">{{price.startPrice}}-{{price.endPrice}}</a><!--动态绑定class是的定义的数等于它的下标，绑定一个店家事件 使得电击的时侯让定义的数等于他的下标-->
           </dd>
         </dl>
       </div>
@@ -31,7 +31,7 @@
           <ul>
             <li v-for="(item,index) in goodsList" :key="index">
               <div class="pic">
-                <a href="#"><img v-lazy="'/static/'+item.productImage" alt=""></a>
+                <a href="#"><img v-lazy="'/static/'+item.productImage" alt=""></a> <!--利用v-lazy的方式来绑定图片从而实现图片懒加载，必须下载插件+-->
               </div>
               <div class="main">
                 <div class="name">{{item.productName}}</div>
@@ -48,7 +48,7 @@
   </div>
     </div>
     <div class="md-overlay" v-show="overLayFlag" @click="closePop"></div>
-    <nav-Footer></nav-Footer> 
+    <nav-Footer></nav-Footer>    <!-- 底部组件-->
   </div>
   
 </template>
@@ -56,10 +56,10 @@
 <script>
 import "./../assets/css/base.css"
 import "./../assets/css/product.css"
-import NavHeader from "@/components/NavHeader.vue"
-import NavFooter from "@/components/NavFooter.vue"
-import NavBread from "@/components/NavBread.vue"
-import axios from "axios"
+import NavHeader from "@/components/NavHeader.vue"//将头部标签导入到根组件
+import NavFooter from "@/components/NavFooter.vue" //将尾部部标签导入到根组件
+import NavBread from "@/components/NavBread.vue"//面包徐标签导入到根组件
+import axios from "axios"  //引入axios文件引入json文件
 export default {
   name: '',
   data() { 
@@ -67,25 +67,16 @@ export default {
     // 这样做的目的：因为vue官网讲解不允许组件之间的状态进行共享，所以每个组件都是独立的
     // 所以data必须是一个函数里面return一个obect
     return {
-      goodsList:[],
+      goodsList:[],    //定义一个空的数组
       // 使排序为true
-      sortFlag:true,
-      page:1,
-      pageSize:8,
+      sortFlag:true, //定义排序
+      page:1,          //页数
+      pageSize:8,  //一页显示多少条数据
 
-      priceFilter:[
-        {
-          startPrice:'0.00',
-          endPrice:'500.00'
-        },
-        {
-           startPrice:'500.00',
-          endPrice:'1000.00'
-        },
-        {
-           startPrice:'1000.00',
-          endPrice:'2000.00'
-        }
+      priceFilter:[  //定义一个数组用来循环价格
+        {startPrice:'0.00',endPrice:'500.00'},
+        {startPrice:'500.00',endPrice:'1000.00'},
+        {startPrice:'1000.00',endPrice:'2000.00'}
       ],
         priceChecked:'all'
     }
@@ -117,33 +108,33 @@ export default {
     },
     // 将axios请求填写到methods中，方法名为getGoodsList
     getGoodsList(){
-      var param = {
-        page:this.page,
-        pageSize:this.pageSize,
-        sort:this.sortFlag?1:-1
+      var param = {               //获取一个参数
+        page:this.page,            //获取页数
+        pageSize:this.pageSize,   
+        sort:this.sortFlag?1:-1    //获取排序,如果是升序，就是1，如果是降序，就是-1
       }
         axios.get("/goods",{
-          param:param
+          params:param         //将定义的对象放入axios中
         }).then((response)=>{
       console.log(response);
         let res = response.data;
        if(res.status=='0'){
-         this.goodsList = res.result.list;
+         this.goodsList = res.result.list;  //否则的话就是以前的状态 也就是填充数据
        }else{
-         this.goodsList = [];
+         this.goodsList = [];    //请求失败,就会不显示，变成了一个空的数组
        }
       }) 
     },
     // 排序进行取反，绑定到上面
     sortGoods(){
-      this.sortFlag = !this.sortFlag;
-      this.page = 1;
-      this.getGoodsList()
+      this.sortFlag = !this.sortFlag;//点击的时候默认升序取反
+      this.page = 1;                  //点击时默认第一页
+      this.getGoodsList()             //重新获取getGoodsList
     }
   },
-  //排序定义方法getGoodsList
+  //排序定义方法getGoodsList ，模板渲染完毕生命周期
   mounted(){
-     this.getGoodsList()
+     this.getGoodsList()//定义一个方法
   },
  
  }
